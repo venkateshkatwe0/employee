451417ec1366473414f3a55bad3f109d84614a59
venkatesh_katwe <01fe24ica045@kletech.ac.in>
2025-11-21 12:20:33 +0530

    my first commit

--- test_employee.py
+++ test_employee.py
@@ -1,18 +1,21 @@
-#test_employee.py
+# test_employee.py
 import pytest
 from employee import get_employee_info
+
 def test_get_employee_info():
-        #Sample data
-        name = "Alice Smith"
-        emp_id="E202"
-        department = "HR"
-        salary = 60000
-        #expected result
-        excepted_output = (
-            "Employee Name: Alice smith,"
-            "Employee ID: E202,"
-            "Department: HR"
-            "Salary: 60000.00"
-        )
-        #Assertion
-        assert get_employee_info(name,emp_id,department,salary) == excepted_output
+    # Sample data
+    name = "Alice Smith"
+    emp_id = "E202"
+    department = "HR"
+    salary = 60000
+
+    # Expected result
+    expected_output = (
+        "Employee Name: Alice Smith\n"
+        "Employee ID: E202\n"
+        "Department: HR\n"
+        "Salary: 60000.00"
+    )
+
+    # Assertion
+    assert get_employee_info(name, emp_id, department, salary) == expected_output
